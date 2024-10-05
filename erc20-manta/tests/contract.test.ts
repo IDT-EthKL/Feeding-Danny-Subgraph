d1460dba106f782596fa437edbddb447ca64c594
Lee Quan Jin <130299519+leequanjin@users.noreply.github.com>
2024-10-06 03:50:58 +0800

    Initialize subgraph

--- erc20-manta/tests/contract.test.ts
+++ erc20-manta/tests/contract.test.ts
@@ -6,28 +6,24 @@ import {
   beforeAll,
   afterAll
 } from "matchstick-as/assembly/index"
-import { BigInt, Address } from "@graphprotocol/graph-ts"
-import { CrossChainInteraction } from "../generated/schema"
-import { CrossChainInteraction as CrossChainInteractionEvent } from "../generated/Contract/Contract"
-import { handleCrossChainInteraction } from "../src/contract"
-import { createCrossChainInteractionEvent } from "./contract-utils"
+import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { Approval } from "../generated/schema"
+import { Approval as ApprovalEvent } from "../generated/Contract/Contract"
+import { handleApproval } from "../src/contract"
+import { createApprovalEvent } from "./contract-utils"
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
 
 describe("Describe entity assertions", () => {
   beforeAll(() => {
-    let originChain = BigInt.fromI32(234)
-    let player = Address.fromString(
+    let owner = Address.fromString("0x0000000000000000000000000000000000000001")
+    let spender = Address.fromString(
       "0x0000000000000000000000000000000000000001"
     )
-    let fishId = BigInt.fromI32(234)
-    let newCrossChainInteractionEvent = createCrossChainInteractionEvent(
-      originChain,
-      player,
-      fishId
-    )
-    handleCrossChainInteraction(newCrossChainInteractionEvent)
+    let value = BigInt.fromI32(234)
+    let newApprovalEvent = createApprovalEvent(owner, spender, value)
+    handleApproval(newApprovalEvent)
   })
 
   afterAll(() => {
@@ -37,26 +33,26 @@ describe("Describe entity assertions", () => {
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
-  test("CrossChainInteraction created and stored", () => {
-    assert.entityCount("CrossChainInteraction", 1)
+  test("Approval created and stored", () => {
+    assert.entityCount("Approval", 1)
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
-      "CrossChainInteraction",
+      "Approval",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
-      "originChain",
-      "234"
-    )
-    assert.fieldEquals(
-      "CrossChainInteraction",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
-      "player",
+      "owner",
       "0x0000000000000000000000000000000000000001"
     )
     assert.fieldEquals(
-      "CrossChainInteraction",
+      "Approval",
       "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
-      "fishId",
+      "spender",
+      "0x0000000000000000000000000000000000000001"
+    )
+    assert.fieldEquals(
+      "Approval",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "value",
       "234"
     )
 
